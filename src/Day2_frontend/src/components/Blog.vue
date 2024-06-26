1f4792fe2f9df9db84a92571a589fdf32586a42a
No-uIsObsolete <andrzejf2007@gmail.com>
2024-06-26 19:15:29 +0200

    Added Tailwing and Style

--- src/Day2_frontend/src/components/Blog.vue
+++ src/Day2_frontend/src/components/Blog.vue
@@ -1,32 +1,45 @@
 <template>
     <div>
-        <button @click = "pobierzWpisy">refresh</button>
+        <button @click="pobierzWpisy">refresh</button>
         siema blog!
         {{ wpisy }}
         <input v-model="nowyBlog" type="text">
-        <button @click = "dodajWpisy">dodaj</button>
-
-
+        <button @click="dodajWpisy">dodaj</button>
+        <h2 class="text-blue-600">Wpisy na bloga:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
+            <button  class="bg-blue-600 rounded text-white p-4" @click="dodajWpisy">dodaj</button>
+        </div>
     </div>
 </template>
 
- <script>
+<script>
 import { Day2_backend } from 'declarations/Day2_backend/index';
-
 export default {
     data() {
         return {
-            wpisy: []
+            wpisy: [],
+            nowyBlog: ""
         }
     },
     methods: {
-        async pobierzWpisy() {
-            this.wpisy = await Day2_backend.dodaj_wpisy(nowyBlog)
-        },
-
         async dodajWpisy() {
-            this.wpisy = await Day2_backend.odczytaj_wpisy()
+            await Day2_backend.dodaj_wpis(this.nowyBlog);
+        },
+        async pobierzWpisy() {
+            this.wpisy = await Day2_backend.odczytaj_wpisy();
         }
     },
+    async mounted(){
+        this.pobierzWpisy()
+    }
 }
 </script>
